da067655216705cdb39693898299a6c4a321a3c9
Nir Buberman <nirbuber@gmail.com>
2025-12-24 18:11:18 +0200

    Implement Node Class for the graph

--- Node.java
+++ Node.java
@@ -9,6 +9,45 @@ public class Node {
     private List<Node> edges;
     private Message msg;
 
+    public Node(String name) {
+        this.name = name;
+        this.edges = new ArrayList<>();
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Node> getEdges() {
+        return edges;
+    }
+    public void setEdges(List<Node> edges) {
+        if (edges == null) {
+            this.edges = new ArrayList<>();
+        }else{
+            this.edges = edges;
+        }
+    }
+
+    public Message getMsg() {
+        return msg;
+    }
+    public void setMsg(Message msg) {
+        this.msg = msg;
+    }
+
+    public void addEdge(Node node) {
+        if (node != null && !edges.contains(node)) {
+            edges.add(node);
+        }
+    }
+   
+    @Override
+    public String toString() {
+        return "Node [name=" + name + ", edges=" + edges + ", msg=" + msg + "]";
+    }
+
 
 
 }
